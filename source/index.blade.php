a864bbaea9ca13191389bf0ce471e3ee40c7bee8
Alex Gustafson <alex@weareadjacent.com>
2025-06-12 12:46:10 -0400

    Testimonials

--- source/index.blade.php
+++ source/index.blade.php
@@ -13,7 +13,7 @@
             <div class="flex my-10">
                 <a href="#contact" title="Contact Alex" class="bg-blue-500 hover:bg-blue-600 font-normal text-white hover:text-white rounded mr-4 py-2 px-6">Contact</a>
 
-                <a href="#other" title="Other" class="bg-gray-300 hover:bg-gray-600 text-blue-900 font-normal hover:text-white rounded py-2 px-6">Other</a>
+{{--                <a href="#testimonials" title="Testimonials" class="bg-gray-300 hover:bg-gray-600 text-blue-900 font-normal hover:text-white rounded py-2 px-6">Testimonials</a>--}}
             </div>
         </div>
 
@@ -22,29 +22,34 @@
 
     <hr class="block my-8 border lg:hidden">
 
+    <h2 id="testimonials">Testimonials</h2>
     <div class="md:flex -mx-2 -mx-4">
         <div class="mb-8 mx-3 px-2 md:w-1/3">
-            <img src="/assets/img/icon-window.svg" class="h-12 w-12" alt="window icon">
+            <img src="/assets/img/brilliant.svg" class="h-20 w-20" alt="brilliant icon">
 
-            <h3 id="intro-laravel" class="text-2xl text-blue-900 mb-0">Templating with <br>Laravel's Blade engine</h3>
+            <h3 id="intro-laravel" class="text-2xl text-blue-900 mb-0">"Alex is brilliant!"</h3>
 
-            <p>Blade is a powerful, simple, and beautiful templating language, and now you can use it for your static sites, not just your Laravel-powered apps.</p>
+            <blockquote>"Alex is brilliant! He managed to cut down a time-consuming project into a quick website, and was delightful to work with all along the way. I will be working with Alex again and encourage you to, as well! He really knows his stuff and finishes projects with high quality and a 360-degree view on the outcome."
+            </blockquote>
+            <cite>Laser Malena-Webber, Doubleclicks Records</cite>
         </div>
 
         <div class="mb-8 mx-3 px-2 md:w-1/3">
-            <img src="/assets/img/icon-terminal.svg" class="h-12 w-12" alt="terminal icon">
+            <img src="/assets/img/exact.svg" class="mt-4 h-12 w-12" alt="exact icon">
 
-            <h3 id="intro-markdown" class="text-2xl text-blue-900 mb-0">Use Markdown for <br>content-driven pages</h3>
+            <h3 id="intro-markdown" class="pt-4 text-2xl text-blue-900 mb-0">"Exactly What We Needed"</h3>
 
-            <p>Markdown is the web’s leading format for writing articles, blog posts, documentation, and more. Jigsaw makes it painless to work with Markdown content.</p>
+            <blockquote>It's a pleasure working with Alex. His front end development expertise on a short timeline was exactly what we needed [...] A professional through the whole process.</blockquote>
+            <cite>Brad Cummings, Founder & CEO of Equilottery Games</cite>
         </div>
 
         <div class="mx-3 px-2 md:w-1/3">
-            <img src="/assets/img/icon-stack.svg" class="h-12 w-12" alt="stack icon">
+            <img src="/assets/img/team.svg" class="mt-4 h-12 w-12" alt="team icon">
 
-            <h3 id="intro-mix" class="text-2xl text-blue-900 mb-0">Compile your assets <br>using Laravel Mix </h3>
+            <h3 id="intro-mix" class="pt-4 text-2xl text-blue-900 mb-0">"A Valuable Contributor"</h3>
 
-            <p>Jigsaw comes pre-configured with Laravel Mix, a simple and powerful build tool. Use the latest frontend tech with just a few lines of code.</p>
+            <blockquote>I am happy to give an enthusiastic recommendation for Alex’s work. His knowledge, experience, and attitude make him a valuable contributor through all phases of a development project, from planning and strategy through execution. On top of his strong work, his communication skills help keep the project moving smoothly and everyone up-to-date.</blockquote>
+            <cite>Jon Reigelman, Creative Director at National Center for Families Learning</cite>
         </div>
     </div>
 </section>
